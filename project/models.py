ebedb7a0b0919a8b2d0f96e6bf2b1cd8bfc4f3ce
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-23 01:30:52 +0300

    Update models.py

--- project/models.py
+++ project/models.py
@@ -6,7 +6,7 @@ class CookBook(Base):
     __tablename__ = "Cookbook"
     __table_args__ = {'extend_existing': True} 
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String, index=True)
     count = Column(Integer, index=True, default=0)
     cook_time = Column(Integer, default=0)
