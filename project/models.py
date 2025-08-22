fb3ce35df934bb371981441a5e67e0de44697b91
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-22 22:42:18 +0300

    Update models.py

--- project/models.py
+++ project/models.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, String, Integer
 
 class CookBook(Base):
     __tablename__ = "Cookbook"
+    __table_args__ = {'extend_existing': True} 
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
