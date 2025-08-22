9d1c2a080f3eee119b94ccdfeccbe844839ef247
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-22 20:38:54 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest_asyncio
 from typing import Any, AsyncGenerator
-from hw1.project import models
+from project import models
 
-from hw1.project.main import app
+from project.main import app
 from httpx import ASGITransport, AsyncClient
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
