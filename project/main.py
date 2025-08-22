784933f904e6b851712884afc93bc73d577ce046
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-23 02:13:38 +0300

    Update main.py

--- project/main.py
+++ project/main.py
@@ -51,9 +51,9 @@ async  def get_recipes_id(recipe_id: int = Path(...,title="id of recipe"))-> mod
 async def add_recipe(recipe: schemas.CookBookIn)-> models.CookBook:
     new_recipe = models.CookBook(name=recipe.name, cook_time=recipe.cook_time,
                                  descript=recipe.descript, ingredients=recipe.ingredients)
-    async with session.begin():
-        await session.add(new_recipe)
-        await session.commit()
+    async with async_session.begin() as session:
+        session.add(new_recipe)
+        session.commit()
         return new_recipe
 
 
