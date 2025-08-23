0f56c19c553284b418c485795d61150525e3f174
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-24 01:56:46 +0300

    Update models.py

--- project/models.py
+++ project/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, String, Integer
-from database import Base  # mypy: enable-error-code="truthy-bool, ignore-without-code"
+from database import Base  # type: ignore[import-not-found]
+# mypy: enable-error-code="truthy-bool, ignore-without-code"
 
 
 
