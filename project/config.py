a7e2703e38b566ea227f856d865692e54d478e1b
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-24 01:14:50 +0300

    Update config.py

--- project/config.py
+++ project/config.py
@@ -1,4 +1,4 @@
-from pydantic-settings import BaseSettings as PydanticSettings
+from pydantic-settings import BaseSettings as PydanticSettings, SettingsConfigDict
 
 class Settings(PydanticSettings):
     
