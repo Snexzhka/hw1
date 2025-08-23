4c34c3b6d4bbf4fa4962b505fcd908c0e0481633
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-24 02:13:05 +0300

    Update schemas.py

--- project/schemas.py
+++ project/schemas.py
@@ -2,17 +2,15 @@ from pydantic import BaseModel, Field, ConfigDict
 
 
 # class Ingredients(BaseModel):
-#     name: str=Field(..., title="name of ingr")
-#     quantity: int=Field(..., title="quantity of ingred")
+#     name: str = Field(..., title="name of ingr")
+#     quantity: int = Field(..., title="quantity of ingred")
 #     cookbook_id: int
 
 class BaseRecipes(BaseModel):
-    name: str=Field(..., title="name")
-    cook_time: int=Field(..., title="time of cook")
+    name: str = Field(..., title="name")
+    cook_time: int = Field(..., title="time of cook")
     ingredients: str = Field(..., title="ingredients")
     descript: str = Field(..., title="description")
-
-
 
 
 class CookBookIn(BaseRecipes):
@@ -21,7 +19,6 @@ class CookBookIn(BaseRecipes):
     model_config = ConfigDict(from_attributes=True)
         
         
-
 class CookBookOut(BaseRecipes):
     id: int
     count: int
