a3a5b437dcba794c9f78c175503d7ddcf854cbcc
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-23 13:44:06 +0300

    Update database.py

--- project/database.py
+++ project/database.py
@@ -1,3 +1,4 @@
+import sqlalchemy
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
