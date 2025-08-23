496de5f526417425fae8c6517902097704d645fb
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-24 02:41:04 +0300

    Update database.py

--- project/database.py
+++ project/database.py
@@ -12,8 +12,3 @@ engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 session = async_session()
 Base = declarative_base()
-
-
-
-
-
