0b938a3d182214e2f9d93b9bbcb031d1b60e15e4
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-23 03:38:44 +0300

    Update main.py

--- project/main.py
+++ project/main.py
@@ -29,8 +29,8 @@ async def recipes() -> List[models.CookBook]:
         res = await async_session.execute(select(models.CookBook).order_by(models.CookBook.count.desc()))
         await async_session.commit()
     result = res.scalars().all()
-    res_dict = [schemas.CookBookOut.model_validate(row) for row in result]
-    return res_dict
+    return [schemas.CookBookOut.model_validate(row) for row in result]
+    
 
 
 @app.get("/recipes/{recipe_id}", response_model=schemas.CookBookOut)
