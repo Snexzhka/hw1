868a8f2704b486698649dda2febf7856a1bacab9
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-24 02:09:22 +0300

    Update database.py

--- project/database.py
+++ project/database.py
@@ -9,9 +9,7 @@ DATABASE_URL = "sqlite+aiosqlite:///./apps.py.db"
 engine = create_async_engine(DATABASE_URL, echo=True)
 # expire_on_commit=False will prevent attributes from being expired
 # after commit.
-async_session = async_sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession
-)
+async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 session = async_session()
 Base = declarative_base()
 
