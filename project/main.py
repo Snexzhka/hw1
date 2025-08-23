36f1950b87e4de71c1e372956cab5d876dc97661
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-24 02:57:55 +0300

    Update main.py

--- project/main.py
+++ project/main.py
@@ -55,9 +55,9 @@ async def add_recipe(recipe: schemas.CookBookIn) -> schemas.CookBookIn:
         descript=recipe.descript,
         cook_time=recipe.cook_time,
         ingredients=recipe.ingredients,
-   )    
-   async with session as async_session:        
+    )    
+    async with session as async_session:        
         async_session.add(new_recipe)
         await async_session.commit()
         res = schemas.CookBookIn.model_validate(new_recipe)
-   return res.model_dump()
+    return res.model_dump()
