fee198814952d21a8dcbdd4032c16f58521392c6
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-24 03:09:32 +0300

    Update models.py

--- project/models.py
+++ project/models.py
@@ -5,8 +5,8 @@ from database import Base  # type: ignore[import-not-found]
 class CookBook(Base):
     __tablename__ = "Cookbook"
 
-    
     __table_args__ = {"extend_existing": True} 
+    
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
