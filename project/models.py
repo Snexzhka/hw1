389d3bafae47ff911f55c03c5beb0bc7aaa593b5
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-24 23:45:45 +0300

    Update models.py

--- project/models.py
+++ project/models.py
@@ -2,14 +2,13 @@ from sqlalchemy import Column, String, Integer
 from database import Base  # type: ignore[import-not-found]
 
 
-class CookBook(Base):  
-    __tablename__ = "Cookbook" 
-    __table_args__ = {"extend_existing": True} 
-    
+class CookBook(Base):
+    __tablename__ = "Cookbook"
+    __table_args__ = {"extend_existing": True}
+
     id = Column(Integer, primary_key=True)
     name = Column(String)
     count = Column(Integer, default=0)
     cook_time = Column(Integer, default=0)
     descript = Column(String)
     ingredients = Column(String, default=" ")
-    
