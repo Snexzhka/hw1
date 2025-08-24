0d8e85b7e0b7164942c520eb39ba8a25cd432790
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-24 20:43:47 +0300

    Update schemas.py

--- project/schemas.py
+++ project/schemas.py
@@ -10,12 +10,13 @@ class BaseRecipes(BaseModel):
 
 
 class CookBookIn(BaseRecipes):
-    ...
-    
+    ...  
     model_config = ConfigDict(from_attributes=True)
         
         
 class CookBookOut(BaseRecipes):
+
+    
     id: int    
     count: int    
     model_config = ConfigDict(from_attributes=True)
