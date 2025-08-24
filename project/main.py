263091fdd456fecb75305c1b38e2b5414d5966f1
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-24 03:12:21 +0300

    Update main.py

--- project/main.py
+++ project/main.py
@@ -49,8 +49,7 @@ async def get_recipes_id(
 
 @app.post("/recipes/", response_model=schemas.CookBookIn)
 async def add_recipe(recipe: schemas.CookBookIn) -> schemas.CookBookIn:
-    new_recipe = models.CookBook(
-        
+    new_recipe = models.CookBook(  
         name=recipe.name, 
         descript=recipe.descript,
         cook_time=recipe.cook_time,
