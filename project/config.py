e680988e73134ea7228abf3a4e8ffa7a7adcaaa5
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-24 03:07:50 +0300

    Update config.py

--- project/config.py
+++ project/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings as PydanticSettings, SettingsConfigDict
 
+
 class Settings(PydanticSettings):
-    
-    
+        
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
