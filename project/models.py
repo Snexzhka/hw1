dcad85db824bcd75588cd2db97dfa79167803ad6
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-25 14:58:38 +0300

    Update models.py

--- project/models.py
+++ project/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String
-from database import Base  # type: ignore[import-not-found]
+from database import Base # type: ignore[import-not-found]
 
 
 class CookBook(Base):
