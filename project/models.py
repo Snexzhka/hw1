f2ca3b5f3b32e2eab18553735131bcad8089cf47
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-25 20:21:44 +0300

    Update models.py

--- project/models.py
+++ project/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Integer, Column, String
 
-from database import Base  # type: ignore[import-not-found]
+from hw1.project.database import Base  # type: ignore[import-not-found]
 
 
 class CookBook(Base):
