098ac0d604c20a3f98a1f5b53d4b167226b8776f
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2025-08-29 22:32:01 +0300

    Update database.py

--- project/database.py
+++ project/database.py
@@ -1,4 +1,6 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession  # isort:skip
+from sqlalchemy.ext.asyncio import async_sessionmaker  # isort:skip
+from sqlalchemy.ext.asyncio import create_async_engine  # isort:skip
 from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = "sqlite+aiosqlite:///./apps.py.db"
@@ -6,7 +8,8 @@ DATABASE_URL = "sqlite+aiosqlite:///./apps.py.db"
 engine = create_async_engine(DATABASE_URL, echo=True)
 # expire_on_commit=False will prevent attributes from being expired
 # after commit.
-async_session = async_sessionmaker(engine, expire_on_commit=False, 
-                                   class_=AsyncSession)
+async_session = async_sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)  # noqa: E501
 
 Base = declarative_base()
